a483ff12e3d9513d9b0e97a38a387f3efb359b2a
TasmiaIshrat <chaad.ishrat@gmail.com>
2022-04-13 03:05:17 +0600

    Model Store
    Model Storing on outfolder support added

--- name_classifier.py
+++ name_classifier.py
@@ -2,6 +2,7 @@ import fire
 
 import pickle
 
+
 import numpy as np
 
 import pandas as pd
@@ -18,7 +19,6 @@ from sklearn.feature_extraction import DictVectorizer
 
 
 
-
     
 
 def save_model(model, out_folder: str):
@@ -27,39 +27,58 @@ def save_model(model, out_folder: str):
     Serialise the model to an output folder 
     """
     
-    filename = 'ner_model.sav'
+    try:
     
-    pickle.dump(model, open(out_folder+'/'+filename, 'wb'))
+        filename = 'ner_model.sav'
+        
+        pickle.dump(model, open(out_folder+'/'+filename, 'wb'))
+        
+    except Exception as e: 
+    
+        print('** Failed to Save NER Model..'+ str(e))
 
 
 def evaluate_model(model, test_data):
+    
     """
     Evaluate your model against the test data.
     """
-    print('\n## Test Data Evaluation:\n')
-    print(classification_report(y_pred=model.predict(test_data['test_set_features']), y_true=test_data['test_set_labels'], labels=test_data['classes']))
+    
+    try:
+    
+        print(classification_report(y_pred=model.predict(test_data['test_set_features']), y_true=test_data['test_set_labels'], labels=test_data['classes']))
+        
+    except Exception as e: 
+    
+        print('** Failed to Evaluate NER Model..'+ str(e))
 
 
 def split_data(data):
+    
     """
     Generate data splits
+    
     """
     
-    #dataset_columns = list(data.columns)
+    try:
+        
+        dependant_variable = list(data.columns)[1]
+        
+        features = data.drop(dependant_variable, axis=1)
+        
+        X = vectorize_dataset(features)
     
-    dependant_variable = list(data.columns)[1]
+        y = data[dependant_variable].values
+        
+        classes = np.unique(y)
+        
+        classes = classes.tolist()
+        
+        return train_test_split(X, y, test_size = 0.33, random_state=0)
     
-    features = data.drop(dependant_variable, axis=1)
+    except Exception as e: 
     
-    X = vectorize_dataset(features)
-
-    y = data[dependant_variable].values
-    
-    classes = np.unique(y)
-    
-    classes = classes.tolist()
-    
-    return train_test_split(X, y, test_size = 0.33, random_state=0)
+        print('** Failed Splitting Dataset..'+ str(e))
     
     
    
@@ -69,24 +88,46 @@ def split_data(data):
 def load_data(in_folder: str):
     
     """
+    
     The in_folder will contain two files:
      - person.ttl
      - name.ttl
 
     You will need to combine the data to generate the y values (0 or 1),
     and train the model (see readme).
+    
     """
     
-    df = pd.read_csv ('in-folder/ner_dataset.csv')
+    try: 
+    
+        df = pd.read_csv (in_folder+'/ner_dataset.csv')
+        
+        return df
+    
+    except Exception as e: 
+    
+        print('** Failed Loading Dataset..'+ str(e))
     
 
-    return df
+    
 
 def vectorize_dataset(features):
     
-    v = DictVectorizer(sparse=False)
+    """
     
-    return v.fit_transform(features.to_dict('records'))
+      Vectorizing nominal features of the dataset
+    
+    """
+    
+    try: 
+        
+        v = DictVectorizer(sparse=False)
+        
+        return v.fit_transform(features.to_dict('records'))
+    
+    except Exception as e: 
+    
+        print('** Failed Vectorizing Dataset..'+ str(e))
     
 
 
@@ -98,38 +139,39 @@ def train(in_folder: str, out_folder: str) -> None:
     and serialise it to the out_folder.
     
     """
-
-    print(in_folder,'  ',out_folder)
     
     dataset = load_data(in_folder)
     
     classes = np.unique(dataset[list(dataset.columns)[1]].values).tolist()
     
+    print('\n## Splitting Dataset into Train & Test Set:\n')
+ 
     X_train, X_test, y_train, y_test = split_data(dataset)
+    
+    print('\nShape of Train Set: '+str(X_train.shape)+',Shape of Test Set: '+str(X_test.shape)+'\n')
+    
+    print('\n## Training Predictive Model:\n')
     
     ner_model = Perceptron(verbose=10, n_jobs=-1, max_iter=5)
     
     ner_model.partial_fit(X_train, y_train, classes)
     
+    print('\n## Training Completed...\n')
+    
+    print('\n## Test Data Evaluation:\n')
+    
     evaluate_model(ner_model, {'test_set_features': X_test, 'test_set_labels': y_test, 'classes': classes.copy()})
+    
+    print('\n## Saving NER Model...\n')
     
     save_model(ner_model,out_folder)
     
-    print(X_train,X_train.shape)
-    
-    print(X_test,X_test.shape)
-    
-    print(y_train,y_train.shape)
-    
-    print(y_test,y_test.shape)
-    
-    print(classes)
-
+    print('\n## NER Model Saved in "'+out_folder+'" directory...\n')
     
 
     
 
-    pass
+    
   
 
 if __name__ == '__main__':
